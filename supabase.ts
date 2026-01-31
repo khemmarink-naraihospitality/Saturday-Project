0b66b24f13091ced564803fa5d1e9d94b79d8b3d
Khemmarin <khemmarin.k@naraihospitality.com>
2026-01-31 22:11:38 +0700

    Fix TS errors for Vercel deployment

--- supabase.ts
+++ supabase.ts
@@ -1,8 +1,7 @@
-
 import { createClient } from '@supabase/supabase-js'
 
-// Hardcoded for Production Fix
-const supabaseUrl = "https://cwogmwyqoavzrbwwgved.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3b2dtd3lxb2F2enJid3dndmVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3NjU3NjYsImV4cCI6MjA4NTM0MTc2Nn0.Exds1Dn1abpdV_jHn7-Vf-B7blGHl2bZLBf7gelMKAc"
+// Use import.meta.env for Vite environment variables
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
